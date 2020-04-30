25e12da7173cf7e72b102ee49de2d3b8f9f45706
Harshna Goratela <harshna.goratela@gmail.com>
2020-04-30 13:31:00 +0530

    Andrew 29-apr changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,11 +4,12 @@ import './Footer.css'
 
 export default () => (
   <div>
-    
+
     <footer className="footer">
       <div className="container taCenter">
         <span>
-          © Copyright {new Date().getFullYear()} All rights reserved. Crafted by{' '}
+          <a href="https://twitter.com/ecomloop" target="_blank">twitter</a> <a href="https://github.com/ecomloop" target="_blank">github</a> <a href="https://www.upwork.com/agencies/~018dcd9aca877342a3" target="_blank">upwork</a> <a target="_blank" href="https://www.linkedin.com/company/ecomloop/">linkedin</a>
+          <br/>© Copyright {new Date().getFullYear()} All rights reserved. Crafted by{' '}
           <a href="https://ecomloop.com">ecomloop</a>.
         </span>
       </div>
